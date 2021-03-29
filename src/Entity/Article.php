f3e0f6d681b5b24b00fc229f91dfeff4823ea2d4
Ilies Ghezal <ghezal-ilies@outlook.fr>
2021-03-29 14:18:25 +0200

    add new migration and two entities

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -8,183 +8,193 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
+ * @ApiResource(
+ *     normalizationContext={"groups"={"article:read"}},
+ *     denormalizationContext={"groups"={"article:write"}},
+ *     collectionOperations={
+ *      "get"={},
+ *      "post"={},
+ *     },
+ *     itemOperations={
+ *       "get"={},
+ *       "put"={},
+ *       "delete"={},
+ *     }
+ * )
+ *
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
  */
-#[ApiResource(
-    denormalizationContext: ["groups" => ["write"], "swagger_definition_name" => "Write"],
-    normalizationContext: ["groups" => ["read"], "swagger_definition_name" => "Read"],
-)]
 class Article
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     *
-     * @Groups("read")
-     */
-    private $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 *
+	 * @Groups("article:read")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     *
-     * @Groups({"read", "write"})
-     */
-    private $title;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 *
+	 * @Groups({"article:read", "article:write"})
+	 */
+	private $title;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     *
-     * @Groups("read")
-     */
-    private $slug;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 *
+	 * @Groups("article:read")
+	 */
+	private $slug;
 
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     *
-     * @Groups({"read", "write"})
-     */
-    private $content;
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 *
+	 * @Groups({"article:read", "article:write"})
+	 */
+	private $content;
 
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     *
-     * @Groups({"read", "write"})
-     */
-    private $picture;
+	/**
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 *
+	 * @Groups({"article:read", "article:write"})
+	 */
+	private $picture;
 
-    /**
-     * @ORM\Column(type="boolean")
-     *
-     * @Groups("read")
-     */
-    private $isPublished;
+	/**
+	 * @ORM\Column(type="boolean")
+	 *
+	 * @Groups("article:read")
+	 */
+	private $isPublished;
 
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     *
-     * @Groups("read")
-     */
-    private $publishedAt;
+	/**
+	 * @ORM\Column(type="datetime", nullable=true)
+	 *
+	 * @Groups("article:read")
+	 */
+	private $publishedAt;
 
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     *
-     * @Groups("read")
-     */
-    private $updatedAt;
+	/**
+	 * @ORM\Column(type="datetime", nullable=true)
+	 *
+	 * @Groups("article:read")
+	 */
+	private $updatedAt;
 
-    /**
-     * @ORM\Column(type="datetime")
-     *
-     * @Groups("read")
-     */
-    private $createdAt;
+	/**
+	 * @ORM\Column(type="datetime")
+	 *
+	 * @Groups("article:read")
+	 */
+	private $createdAt;
 
-    public function __construct()
-    {
-        $this->isPublished = false;
-        $this->createdAt = new \DateTime();
-    }
+	public function __construct()
+	{
+		$this->isPublished = false;
+		$this->createdAt = new \DateTime();
+	}
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+	public function getTitle(): ?string
+	{
+		return $this->title;
+	}
 
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
+	public function setTitle(string $title): self
+	{
+		$this->title = $title;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
+	public function getSlug(): ?string
+	{
+		return $this->slug;
+	}
 
-    public function setSlug(string $slug): self
-    {
-        $this->slug = $slug;
+	public function setSlug(string $slug): self
+	{
+		$this->slug = $slug;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
+	public function getContent(): ?string
+	{
+		return $this->content;
+	}
 
-    public function setContent(string $content): self
-    {
-        $this->content = $content;
+	public function setContent(string $content): self
+	{
+		$this->content = $content;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPicture(): ?string
-    {
-        return $this->picture;
-    }
+	public function getPicture(): ?string
+	{
+		return $this->picture;
+	}
 
-    public function setPicture(?string $picture): self
-    {
-        $this->picture = $picture;
+	public function setPicture(?string $picture): self
+	{
+		$this->picture = $picture;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getIsPublished(): ?bool
-    {
-        return $this->isPublished;
-    }
+	public function getIsPublished(): ?bool
+	{
+		return $this->isPublished;
+	}
 
-    public function setIsPublished(bool $isPublished): self
-    {
-        $this->isPublished = $isPublished;
+	public function setIsPublished(bool $isPublished): self
+	{
+		$this->isPublished = $isPublished;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPublishedAt(): ?\DateTimeInterface
-    {
-        return $this->publishedAt;
-    }
+	public function getPublishedAt(): ?\DateTimeInterface
+	{
+		return $this->publishedAt;
+	}
 
-    public function setPublishedAt(\DateTimeInterface $publishedAt): self
-    {
-        $this->publishedAt = $publishedAt;
+	public function setPublishedAt(\DateTimeInterface $publishedAt): self
+	{
+		$this->publishedAt = $publishedAt;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
+	public function getUpdatedAt(): ?\DateTimeInterface
+	{
+		return $this->updatedAt;
+	}
 
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
+	public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+	{
+		$this->updatedAt = $updatedAt;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
+	public function getCreatedAt(): ?\DateTimeInterface
+	{
+		return $this->createdAt;
+	}
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
+	public function setCreatedAt(\DateTimeInterface $createdAt): self
+	{
+		$this->createdAt = $createdAt;
 
-        return $this;
-    }
+		return $this;
+	}
 }
